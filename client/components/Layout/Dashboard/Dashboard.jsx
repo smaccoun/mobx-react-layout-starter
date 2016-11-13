0fc523ae944b46a1ec4cb0f7869c491198525bd3
Steven MacCoun <theraccoun@gmail.com>
2016-11-12 20:48:50 -0800

    Username and password input types on login

--- client/components/Layout/Dashboard/Dashboard.jsx
+++ client/components/Layout/Dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {toJS} from 'mobx'
 import {observer} from 'mobx-react'
 
 import routes from '../../../../routes/routes'
@@ -7,8 +8,11 @@ import Navbar from '../Navbar/Navbar'
 import LeftSideMenu from '../LeftSideMenu/LeftSideMenu'
 
 const Dashboard = observer(['store'], (props) => {
+  console.log(toJS(props.store.auth.user.username))
+
   return(
       <div>
+        <h1>Welcome {props.store.auth.user.username}</h1>
         <Navbar/>
         <LeftSideMenu links={[{label: 'main', view: routes.main}]}/>
         <button onClick={() => props.store.router.goTo(routes.home)}> Home Page</button>
--- client/components/Login/Login.jsx
+++ client/components/Login/Login.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {observable} from 'mobx'
 import {observer} from 'mobx-react'
 
 const Login = (props) => {
+
+  let username = observable(null);
+  let password = observable(null);
+
+  const onUserInputChange = (e) => {
+    username = e.target.value
+    console.log(username)
+  }
+
   return(
-      <button onClick={() => props.store.auth.login('Steve')}>Login</button>
+      <div>
+        Username: <input onChange={onUserInputChange}/>
+        Password: <input type="password" onChange={(e) => password = e.target.value}/>
+        <button onClick={() => props.store.auth.login(username, password)}>Login</button>
+      </div>
   )
 }
 
